c4dfaf6702052c416fbe83a55df39a2f706f3e13
Jakub Knapik <jakubknapik3@gmail.com>
2019-01-23 19:53:12 +0100

    Testing Add Book setup

---
+++ __tests__/AddBook.test.js
@@ -0,0 +1,11 @@
+import { mount, createLocalVue, shallowMount } from '@vue/test-utils'
+import AddBook from './../src/components/AddBook'
+import Vuetify from 'vuetify'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+
+describe('AddBook Component unit testing', () => {
+  test('bu', () => {
+    expect(true).toBe(true)
+  })
+})
